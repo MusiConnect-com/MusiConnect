86161adde22249dbde5c984da303b243a003e079
João Víctor DS <diassouzajoao2@gmail.com>
2024-07-03 17:23:10 -0400

    commit overlay / close profileList
    Adicionei o overlay na home-musico, funções para fechar o perfil lista ao clicar no overlay ou nos itens da lista.

--- Home-Musico/Code/HomeMusico.js
+++ Home-Musico/Code/HomeMusico.js
@@ -1,7 +1,9 @@
 const profileButton = document.querySelector(".profile-button");
 const profileList = document.querySelector(".profile-list");
+const profileItems = document.querySelectorAll(".profile-list-items");
 const leave = document.getElementById("leave");
 const buttonSearch = document.querySelector(".button-search");
+const overlay = document.querySelector(".overlay");
 
 buttonSearch.addEventListener("click", function() {
     window.location.href = "/Anúncios/Code/Anuncios.html"
@@ -14,19 +16,31 @@ leave.addEventListener("click", function() {
 function openProfileList(){
     if (profileList.classList.contains("open-profile")){
         profileList.style.display = "none";
+        overlay.style.display = "none";
         profileList.classList.remove("open-profile")
         profileButton.classList.remove("open-profile")
     } else {
-        profileList.style.display = "flex"
+        profileList.style.display = "flex";
+        overlay.style.display = "block";
         profileList.classList.add("open-profile")
         profileButton.classList.add("open-profile")
     }
-    
 }
+profileItems.forEach(items => {
+    items.addEventListener("click", function() {
+        profileList.style.display = "none";
+        overlay.style.display = "none";
+        profileList.classList.remove("open-profile")
+        profileButton.classList.remove("open-profile")
+    })
+})
 
-profileButton.addEventListener("click", openProfileList);
-profileList.addEventListener("click", function() {
+overlay.addEventListener("click", function() {
     profileList.style.display = "none";
+    overlay.style.display = "none";
     profileList.classList.remove("open-profile")
     profileButton.classList.remove("open-profile")
 })
+
+
+profileButton.addEventListener("click", openProfileList);
