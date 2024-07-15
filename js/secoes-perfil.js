4a33e94b7b2c142531c91b8c92f643a34028b9b4
João Víctor DS <diassouzajoao2@gmail.com>
2024-07-15 12:58:03 -0400

    commit secoes-perfil.js / transitions
    Criação do código para alternar entre as seções do perfil. Criação de transições suaves.

---
+++ js/secoes-perfil.js
@@ -0,0 +1,28 @@
+const options = document.querySelectorAll(".nav-perfil ul li span");
+const sections = document.querySelectorAll(".info-perfil section");
+
+options.forEach((option, i) => {
+    const optionsList = document.querySelectorAll(".nav-perfil ul li");
+
+    option.addEventListener("click", function(){
+        optionsList.forEach(optionList => {
+            optionList.classList.remove("selected")
+        });
+        optionsList[i].classList.add("selected")
+
+        sections.forEach(section => {
+            section.classList.remove("active");
+        });
+        const activeSection = sections[i];
+        
+        activeSection.style.display = 'flex';
+        activeSection.offsetHeight;
+        activeSection.classList.add("active");
+        
+        sections.forEach(section => {
+            if (!section.classList.contains('active')) {
+                section.style.display = 'none';
+            }
+        });
+    });
+})
